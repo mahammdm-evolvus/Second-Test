d71eb73a1ce8939429a9bbe6f212b374b518272c
mahammdm-evolvus <Mahammd.m@evolvussolutions.com>
2022-05-23 10:44:33 +0530

    My first Commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.Evolvus.Solutions;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("-----Hello From Maven First Project-----");
+
+	}
+
+}
